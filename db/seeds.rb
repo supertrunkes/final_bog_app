62ca83227749e81aeff49075e33649b848be9906
supertrunkes <supertrunkes@gmail.com>
2017-09-17 18:22:51 -0700

    finished up to part 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Creature.create({name: "luke", description: "jedi"})
+
+Creature.create({name: "vader", description: "bad guy"})
